979c5d45f9afd5bf639bb6ac4d1e2a7e8bdc603b
pdh4869 <pdh4869@gmail.com>
2023-11-21 17:34:33 +0900

    quiz

--- Java_Project/Day03/ex01/control/QuizIf.java
+++ Java_Project/Day03/ex01/control/QuizIf.java
@@ -19,10 +19,14 @@ public class QuizIf {
 		int year = sc.nextInt();
 		
 		if (year % 4 == 0) {
-			if ((year % 100 != 0) || (year % 400 == 0)) {
-				System.out.println("윤년");
-			} else {
+			if (year % 100 == 0) {
 				System.out.println("평년");
+			} else {
+				if (year % 400 == 0) {	
+					System.out.println("윤년");
+				} else {
+					System.out.println("평년");
+				}
 			}
 		} else {
 			System.out.println("평년");
